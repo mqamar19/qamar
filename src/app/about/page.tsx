59f579eb6afb13af7e755dcfef628a74fb8414b2
Muhammad Qamar Sharif <qamartaibacreations@gmail.com>
2023-09-22 17:30:48 +0500

    check merge

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 import { useState } from 'react'
-import Button from '@/Componants/sidebarbutton'
 import Sidebar from '@/Componants/sidebar'
 export default function About() {
   return (
   <>
 
-      <Button/>
+      <Sidebar/>
   <div>
     <p>This the about page by qamar</p>
   </div>
